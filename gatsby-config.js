57403bdec03ba0f1fed7bddc47b9bbf581cbf2b5
honohunter <bbbooo1994@gmail.com>
2020-07-29 21:49:05 +0100

    fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
+        // icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-material-ui`,
